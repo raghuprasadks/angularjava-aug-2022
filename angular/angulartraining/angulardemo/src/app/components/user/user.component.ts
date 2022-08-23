2c27716e4a2d522c6d7aef2147bc4cc5e3074141
raghu prasad ks <prasadraghuks@gmail.com>
2022-08-23 15:12:17 +0530

    updated

--- angular/angulartraining/angulardemo/src/app/components/user/user.component.ts
+++ angular/angulartraining/angulardemo/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-user',
@@ -7,17 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  /**
   name:string="";
   email:string="";
   mobile:number=0;
   location:string="";
-  constructor() { }
+  */
 
+  user:User={
+    name:"",
+    email:"",
+    mobile:0,
+    location:""
+  }
+  constructor() { }
   ngOnInit(): void {
+    /**
     this.name = "Ranga";
     this.email="ranga@gmail.com";
     this.mobile=9000000;
     this.location="Bengaluru"
+ */
+
+    this.user={
+      name:"suresh",
+      email:"suresh@gmail.com",
+      mobile:999999,
+      location:"Mysore"
+    }
   }
 
 }
---
+++ angular/angulartraining/angulardemo/src/app/models/User.ts
@@ -0,0 +1,6 @@
+export interface User{
+    name:string;
+    email:string;
+    mobile:number;
+    location:string;
+}
