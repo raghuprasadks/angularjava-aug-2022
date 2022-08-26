89f00bdd563561cc909f8c25d9f66b4bb795c633
raghu prasad ks <prasadraghuks@gmail.com>
2022-08-26 16:00:59 +0530

    updated

--- angular/angulartraining/angulardemo/src/app/components/templateform/templateform.component.ts
+++ angular/angulartraining/angulardemo/src/app/components/templateform/templateform.component.ts
@@ -18,6 +18,7 @@ export class TemplateformComponent implements OnInit {
     console.log("TemplateformComponent:signUp")
     console.log("TemplateformComponent:data",data.value)
     console.log("TemplateformComponent:data",data.value.email)
+    console.log("data.controls.email.value",data.controls['email'].value)
   }
 
 }
