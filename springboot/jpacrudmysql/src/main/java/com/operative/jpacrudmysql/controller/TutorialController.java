c695fd828993e5df850628cd4834abb46add8c30
raghu prasad <prasadraghuks@gmail.com>
2022-09-07 17:13:19 +0530

    updated

--- springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/controller/TutorialController.java
+++ springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/controller/TutorialController.java
@@ -5,10 +5,11 @@ import com.operative.jpacrudmysql.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -26,6 +27,80 @@ public class TutorialController {
         }
         //return _tutorial;
     }
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required=false) String name){
+        List<Tutorial> tutorials = new ArrayList<Tutorial>();
+        try{
+            if(name==null)
+            {
+                tutorialRepository.findAll().forEach(tutorials::add);
+            }else{
+                tutorialRepository.findByNameContaining(name).forEach(tutorials::add);
+            }
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") Long id){
+        Optional<Tutorial> tutorialData =tutorialRepository.findById(id);
+        if(tutorialData.isPresent()){
+            return new ResponseEntity<>(tutorialData.get(),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+        if (tutorialData.isPresent()) {
+            Tutorial _tutorial = tutorialData.get();
+            _tutorial.setName(tutorial.getName());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+        try {
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<Tutorial>> findByPublished() {
+        try {
+            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 
 }
--- springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/repository/TutorialRepository.java
+++ springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/repository/TutorialRepository.java
@@ -3,5 +3,9 @@ package com.operative.jpacrudmysql.repository;
 import com.operative.jpacrudmysql.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
+    List<Tutorial> findByNameContaining(String title);
+    List<Tutorial> findByPublished(boolean b);
 }
