d3cf0ec473924f74a07da84598e35c22a242b8e6
raghu prasad <prasadraghuks@gmail.com>
2022-09-07 18:27:28 +0530

    updated

--- springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/controller/TutorialController.java
+++ springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/controller/TutorialController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
